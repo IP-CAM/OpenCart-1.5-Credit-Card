178dc8ceda12cd2b5adc674edfc88f917f9b96d6
Jamal Eddine <Jamalc0m@users.noreply.github.com>
2017-02-05 11:43:15 +0300

    Add files via upload

---
+++ admin/language/english/payment/paytabs.php
@@ -0,0 +1,13 @@
+<?php
+// Heading
+$_['heading_title']         = ' Paytabs';
+
+// Text
+$_['text_payment']          = 'Payment';
+$_['text_paytabs']          = '<a href="https://www.paytabs.com/" taget="_blank"><img src="https://www.paytabs.com/theme/frontend_v2/img/logo_blue.png" height="30px" width="110px" alt="Paytabs" title="Paytabs" style="border: 1px solid #EEEEEE;" /></a>';
+
+$_['text_success']          = 'Success: You have modified Paytabs Payment module!';
+$_['text_paytabs_invoice']   = '<img src="https://www.paytabs.com/theme/frontend_v2/img/logo_blue.png" height="30px" width="110px"  alt="Paytabs Payment" title="Paytabs" style="border: 1px solid #EEEEEE;"/> Paytabs Payment</a>';
+
+
+?>
---
+++ catalog/language/english/checkout/error.php
@@ -0,0 +1,9 @@
+<?php
+// Heading
+$_['heading_title'] = 'Error :';
+
+// Text
+$_['text_customer'] = '<p> The Payment is rejected or canceled<p>';
+$_['text_checkout'] = 'Checkout';
+$_['text_guest']    = '<p> The Payment is rejected or canceled<p>';
+?>
